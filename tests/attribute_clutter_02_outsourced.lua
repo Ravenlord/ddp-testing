0dd8d3a38135c595084be1365306544b9c8cf9c5
Markus Deutschl <deutschl.markus@gmail.com>
2014-11-20 17:15:33 +0100

    Refatored preparation for test data, added benchmark for "Attribute Clutter".

--- tests/attribute_clutter_02_outsourced.lua
+++ tests/attribute_clutter_02_outsourced.lua
@@ -2,54 +2,56 @@ pathtest = string.match(test, "(.*/)") or ""
 
 dofile(pathtest .. "common.lua")
 
+-- Dummy function to prevent errors if this file is accidentally run as cleanup task.
+function cleanup()
+  return 0
+end
+
+-- Prepare the test data tables, which can then be reused by the tests.
 function prepare_data()
   local query
   prepare_departments('emp_dep', 1000)
-  prepare_images('emp_img', 1000, '/tmp/image.png')
+  prepare_images('employee_profiles', 1000, '/tmp/image.png')
   prepare_person_names('emp_nam', 1000)
   prepare_phone_numbers('emp_pho', 1000)
-  prepare_salaries('emp_sal', 100, 10, '0.75')
+  prepare_salaries('employee_accounting', 100, 10, '0.75')
 
-  -- Create the real employees table.
+
+  -- Create the employees table.
   query = [[
 CREATE TABLE `employees` (
   `id` INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `department` VARCHAR(255) NOT NULL,
-  `phone` INTEGER(5) UNSIGNED NOT NULL,
-  `image` MEDIUMBLOB,
-  `base_salary` NUMERIC(7,2) NOT NULL,
-  `bonus` NUMERIC(7,2) NOT NULL,
-  `tax_rate` NUMERIC(3,2) NOT NULL,
+  `phone` INTEGER(5) NOT NULL,
   INDEX `employees_departments` (`department`)
 )
 ]]
   db_query(query)
   -- Insert test data by joining.
   query = [[
-INSERT INTO `employees` (`name`, `department`, `phone`, `image`, `base_salary`, `bonus`, `tax_rate`)
+INSERT INTO `employees` (`name`, `department`, `phone`)
   SELECT
     `emp_nam`.`name`,
     `emp_dep`.`department`,
-    `emp_pho`.`phone`,
-    `emp_img`.`image`,
-    `emp_sal`.`base_salary`,
-    `emp_sal`.`bonus`,
-    `emp_sal`.`tax_rate`
+    `emp_pho`.`phone`
   FROM `emp_nam`
     INNER JOIN `emp_dep` ON `emp_dep`.`id` = `emp_nam`.`id`
     INNER JOIN `emp_pho` ON `emp_pho`.`id` = `emp_nam`.`id`
-    INNER JOIN `emp_img` ON `emp_img`.`id` = `emp_nam`.`id`
-    INNER JOIN `emp_sal` ON `emp_sal`.`id` = `emp_nam`.`id`
 ]]
   db_query(query)
 
-  -- Delete unnecessary tables.
+  -- Delete null rows from images table.
+  db_query('DELETE FROM `employee_profiles` WHERE `image` IS NULL')
+
+  -- Create foreign keys on dependent tables.
+  db_query('ALTER TABLE `employee_accounting` ADD FOREIGN KEY (`id`) REFERENCES `employees`(`id`) ON DELETE CASCADE ON UPDATE CASCADE')
+  db_query('ALTER TABLE `employee_profiles` ADD FOREIGN KEY (`id`) REFERENCES `employees`(`id`) ON DELETE CASCADE ON UPDATE CASCADE')
+
+  -- Drop unnecessary tables.
   drop_table('emp_dep')
-  drop_table('emp_img')
   drop_table('emp_nam')
   drop_table('emp_pho')
-  drop_table('emp_sal')
 end
 
 function event(thread_id)
--- tests/common.lua
+++ tests/common.lua
@@ -1,3 +1,152 @@
+-- Global array for easy access.
+departments = {
+   'Accounting',
+   'Customer Services',
+   'Finance',
+   'Human Resources',
+   'IT',
+   'Management',
+   'Operations',
+   'Quality Assurance',
+   'Research and Development',
+   'Sales'
+}
+
+--- Drop a database table.
+-- @param name  The table name to drop.
+function drop_table(name)
+  db_query('DROP TABLE `' .. name .. '`')
+end
+
+--- Generic prepare function used for timing.
+function prepare()
+  set_vars()
+  local time
+  time = os.date("*t")
+  print(("%02d:%02d:%02d"):format(time.hour, time.min, time.sec) .. ' - Preparing test data')
+  prepare_data()
+  time = os.date("*t")
+  print(("%02d:%02d:%02d"):format(time.hour, time.min, time.sec) .. ' - Preparation complete')
+end
+
+--- Prepare a table with department names randomly distributed (uniform).
+-- @param   name     The name of the table.
+-- @param   number   Defines how many rows will be generated.
+function prepare_departments(name, number)
+   local query, size
+   query = [[
+CREATE TABLE `]] .. name .. [[` (
+  `id` INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  `department` VARCHAR(255) NOT NULL
+)
+]]
+   db_query(query)
+   -- Insert the departments.
+   size = table.getn(departments)
+   db_bulk_insert_init('INSERT INTO `' .. name .. '` (`department`) VALUES')
+   for i = 1, number do
+      db_bulk_insert_next("('" .. departments[sb_rand_uniform(1,size)] .. "')")
+   end
+   db_bulk_insert_done()
+end
+
+--- Prepare a table for images with 80% images present.
+-- @param name     The name of the table.
+-- @param number   Defines how many rows will be generated.
+-- @param path     The path to the image.
+function prepare_images(name, number, path)
+  local image, image_value, query
+  query = [[
+CREATE TABLE `]] .. name .. [[` (
+  `id` INTEGER UNSIGNED PRIMARY KEY,
+  `image` MEDIUMBLOB
+)
+]]
+  db_query(query)
+  -- Insert the images, 20% of the rows will have NULL values.
+  image = "LOAD_FILE('" .. path .. "')"
+  db_bulk_insert_init('INSERT INTO `'.. name .. '` (`id`, `image`) VALUES')
+  for i = 1, number do
+    if i % 5 == 0 then
+      image_value = 'NULL'
+    else
+      image_value = image
+    end
+    db_bulk_insert_next("(" .. i .. ", " .. image_value .. ")")
+  end
+  db_bulk_insert_done()
+end
+
+--- Prepare a table with person names.
+-- @param name     The name of the table.
+-- @param number   Defines how many names will be generated.
+function prepare_person_names(name, number)
+  local query
+  query = [[
+CREATE TABLE `]] .. name .. [[` (
+  `id` INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL
+)
+]]
+  db_query(query)
+  query = [[
+INSERT INTO `]] .. name .. [[` (`name`)
+  SELECT DISTINCT `name` FROM `]] .. schema_data .. [[`.`names` LIMIT ]] .. number
+  db_query(query)
+end
+
+--- Prepare a table with phone numbers (length 5).
+-- @param name     The name of the table.
+-- @param number   Defines how many phone numbers will be generated.
+function prepare_phone_numbers(name, number)
+  local query
+  query = [[
+CREATE TABLE `]] .. name .. [[` (
+  `id` INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  `phone` INTEGER(5) NOT NULL
+)
+]]
+  db_query(query)
+  query = [[
+INSERT INTO `]] .. name .. [[` (`phone`)
+  SELECT `value` FROM `]] .. schema_data .. [[`.`integers` WHERE `value` < 100000 ORDER BY RAND() LIMIT ]] .. number
+  db_query(query)
+end
+
+--- Prepare a table basic salary information.
+-- Consists of the fields 'base_salary', 'bonus' and 'tax_rate' (fixed).
+-- The total number of rows is determined by num_base_salaries * num_boni,
+-- which is a permutation of random fixed point numbers.
+-- @param name              The name of the table.
+-- @param num_base_salaries Defines how many random base salaries will be generated.
+-- @param num_boni          Defines how many random boni will be generated.
+-- @param tax_rate          The fixed tax rate to insert.
+function prepare_salaries(name, num_base_salaries, num_boni, tax_rate)
+  local query
+  query = [[
+CREATE TABLE `]] .. name .. [[` (
+  `id` INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  `base_salary` NUMERIC(7,2) NOT NULL,
+  `bonus` NUMERIC(7,2) NOT NULL,
+  `tax_rate` NUMERIC(3,2) NOT NULL
+)
+]]
+  db_query(query)
+  -- Insert the pseudo-random combinations.
+  query = [[
+INSERT INTO `]] .. name .. [[` (`base_salary`, `bonus`, `tax_rate`)
+  SELECT `num1`.`value`, `num2`.`value`, ]] .. tax_rate .. [[
+    FROM (
+      SELECT `value` FROM `]] .. schema_data .. [[`.`numerics` ORDER BY RAND() LIMIT ]] .. num_base_salaries .. [[
+    ) AS `num1`
+    CROSS JOIN (
+      SELECT `value` FROM `]] .. schema_data .. [[`.`numerics` ORDER BY RAND() LIMIT ]] .. num_boni .. [[
+    ) AS `num2`
+]]
+  db_query(query)
+end
+
+--- Set variables provided on the command line.
 function set_vars()
    oltp_table_size = oltp_table_size or 10000
    oltp_range_size = oltp_range_size or 100
@@ -29,4 +178,9 @@ function set_vars()
       oltp_skip_trx = false
    end
 
+end
+
+--- Initialize benchmark threads.
+function thread_init(thread_id)
+  set_vars()
 end
