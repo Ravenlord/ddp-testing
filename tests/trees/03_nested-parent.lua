6b83ce17ed538ebebf4d8ca3bcdd847051df8ce0
Markus Deutschl <deutschl.markus@gmail.com>
2015-04-30 15:57:02 +0200

    Implemented Trees tests for direct parent queries.

---
+++ tests/trees/03_nested-parent.lua
@@ -0,0 +1,55 @@
+--[[!
+ - This is free and unencumbered software released into the public domain.
+ -
+ - Anyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form
+ - or as a compiled binary, for any purpose, commercial or non-commercial, and by any means.
+ -
+ - In jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright
+ - interest in the software to the public domain. We make this dedication for the benefit of the public at large and to
+ - the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in
+ - perpetuity of all present and future rights to this software under copyright law.
+ -
+ - THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ - WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE
+ - LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ - OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ -
+ - For more information, please refer to <http://unlicense.org/>
+--]]
+
+--[[
+ - Benchmark file for design problem "Trees", "Nested Sets" solution.
+ - Select direct parent of "cat".
+ -
+ - @author Markus Deutschl <deutschl.markus@gmail.com>
+ - @copyright 2014 Markus Deutschl
+ - @license http://unlicense.org/ Unlicense
+--]]
+
+
+-- --------------------------------------------------------------------------------------------------------------------- Includes
+
+
+pathtest = string.match(test, "(.*/)") or ""
+
+-- Total reuse of preparations.
+dofile(pathtest .. "03_nested-insert.lua")
+
+
+-- --------------------------------------------------------------------------------------------------------------------- Benchmark functions
+
+
+--- Execute the benchmark queries.
+-- Is called during the run command of sysbench.
+function benchmark()
+  rs = db_query([[
+SELECT `parent`.*
+FROM `animals` AS `child`
+  INNER JOIN `animals` AS `parent`
+    ON `child`.`left` > `parent`.`left` AND `child`.`left` < `parent`.`right`
+  LEFT JOIN `animals` AS `intermediate`
+    ON `child`.`left` > `intermediate`.`left` AND `child`.`left` < `intermediate`.`right`
+    AND `intermediate`.`left` > `parent`.`left` AND `intermediate`.`left` < `parent`.`right`
+WHERE `child`.`id` = 3 AND `intermediate`.`id` IS NULL
+]])
+end
