340e740958fd85bfbabbefbe6d9b4725f7cb0e97
Markus Deutschl <deutschl.markus@gmail.com>
2015-04-30 12:29:53 +0200

    Implemented Calculated Values (row-derived) tests for inserts.

---
+++ tests/calculated_values/row-derived/01_trivial-insert.lua
@@ -0,0 +1,63 @@
+--[[!
+ - This is free and unencumbered software released into the public domain.
+ -
+ - Anyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form
+ - or as a compiled binary, for any purpose, commercial or non-commercial, and by any means.
+ -
+ - In jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright
+ - interest in the software to the public domain. We make this dedication for the benefit of the public at large and to
+ - the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in
+ - perpetuity of all present and future rights to this software under copyright law.
+ -
+ - THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ - WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE
+ - LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ - OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ -
+ - For more information, please refer to <http://unlicense.org/>
+--]]
+
+--[[
+ - Benchmark file for design problem "Calculated Values", trivial solution.
+ - Insert new product.
+ -
+ - @author Markus Deutschl <deutschl.markus@gmail.com>
+ - @copyright 2014 Markus Deutschl
+ - @license http://unlicense.org/ Unlicense
+--]]
+
+
+-- --------------------------------------------------------------------------------------------------------------------- Includes
+
+
+pathtest = string.match(test, "(.*/)") or ""
+
+dofile(pathtest .. "../../common.inc")
+dofile(pathtest .. "prepare.inc")
+
+
+-- --------------------------------------------------------------------------------------------------------------------- Preparation functions
+
+
+--- Prepare data for the benchmark.
+-- Is called during the prepare command of sysbench in common.lua.
+function prepare_data()
+  -- Reuse data preparation.
+  prepare_row_derived()
+end
+
+
+-- --------------------------------------------------------------------------------------------------------------------- Benchmark functions
+
+
+--- Execute the benchmark queries.
+-- Is called during the run command of sysbench.
+function benchmark()
+  local query = [[
+INSERT INTO `products` (`name`, `description`, `base_price`, `vat_rate`)
+  VALUES ('New Product', 'New Description', 10.0, 0.2)
+]]
+  db_query('BEGIN')
+  db_query(query)
+  db_query('ROLLBACK')
+end
